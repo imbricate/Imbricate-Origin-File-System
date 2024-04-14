a025a3d7d50f31a0fc65ed58a37de95a9e6a4747
wmxpy <wm@mengw.io>
2024-04-13 20:45:58 -0500

    Add createQueryScriptsFeature function to prepareFileSystemFeatures

--- src/features/prepare.ts
+++ src/features/prepare.ts
@@ -7,6 +7,7 @@
 import { IImbricateOrigin, SandboxFeature } from "@imbricate/core";
 import { createCreatePageFeature } from "./create-page";
 import { createQueryPagesFeature } from "./query-pages";
+import { createQueryScriptsFeature } from "./query-scripts";
 import { createSearchPagesFeature } from "./search-pages";
 import { createSearchScriptsFeature } from "./search-scripts";
 
@@ -17,6 +18,7 @@ export const prepareFileSystemFeatures = (
     const originFeatures: SandboxFeature[] = [
         createCreatePageFeature(origin),
         createQueryPagesFeature(origin),
+        createQueryScriptsFeature(origin),
         createSearchPagesFeature(origin),
         createSearchScriptsFeature(origin),
     ];
---
+++ src/features/query-scripts.ts
@@ -0,0 +1,69 @@
+/**
+ * @author WMXPY
+ * @namespace Features
+ * @description Query Scripts
+ */
+
+import { IImbricateOrigin, IImbricateScript, ImbricateScriptAttributes, SandboxFeature, SandboxFeatureBuilder } from "@imbricate/core";
+import { FileSystemScriptMetadata } from "../script/definition";
+
+type QueryScriptsInput = {
+
+    readonly collection: string;
+
+    readonly limit?: number;
+};
+
+type QueryScriptsOutput = {
+
+    readonly scripts: FileSystemScriptMetadata[];
+};
+
+const createImplementation = (
+    origin: IImbricateOrigin,
+) => {
+
+    return async (
+        input: QueryScriptsInput,
+    ): Promise<QueryScriptsOutput> => {
+
+        const scripts: IImbricateScript[] = await origin.queryScripts({
+            limit: input.limit,
+        });
+
+        const results: FileSystemScriptMetadata[] = [];
+
+        for (const script of scripts) {
+
+            const attributes: ImbricateScriptAttributes = await script.readAttributes();
+
+            results.push({
+
+                scriptName: script.scriptName,
+                identifier: script.identifier,
+
+                description: script.description,
+
+                createdAt: script.createdAt,
+                updatedAt: script.updatedAt,
+
+                attributes,
+            });
+        }
+
+        return {
+            scripts: results,
+        };
+    };
+};
+
+export const createQueryScriptsFeature = (
+    origin: IImbricateOrigin,
+): SandboxFeature => {
+
+    return SandboxFeatureBuilder.providedByOrigin()
+        .withPackageName("script")
+        .withMethodName("queryScripts")
+        .withImplementation(createImplementation(origin))
+        .build();
+};
