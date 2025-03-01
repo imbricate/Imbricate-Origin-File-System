e48acab18b1a4cca39d75af81d20cbfa96c33296
wmxpy <wm@mengw.io>
2025-03-01 13:02:27 -0600

    add base path related actions

---
+++ src/origin/origin-action/get-base-path.ts
@@ -0,0 +1,16 @@
+/**
+ * @author WMXPY
+ * @namespace Origin_OriginAction
+ * @description Get Base Path
+ */
+
+import { ImbricateOriginActionResultOutput } from "@imbricate/core";
+
+export const originExecuteGetBasePathOriginAction = (
+    parameters: Record<string, any>,
+): ImbricateOriginActionResultOutput => {
+
+    return {
+        content: "Hello World",
+    };
+};
---
+++ src/origin/origin-actions.ts
@@ -0,0 +1,28 @@
+/**
+ * @author WMXPY
+ * @namespace Origin
+ * @description Origin Actions
+ */
+
+import { IMBRICATE_ORIGIN_ACTION_APPEARANCE, ImbricateOriginAction } from "@imbricate/core";
+import { IETF_LOCALE } from "@sudoo/locale";
+
+export const getOriginOriginAction = (): ImbricateOriginAction[] => {
+
+    return [
+        {
+            actionIdentifier: "get-origin-action",
+            defaultLocale: IETF_LOCALE.ENGLISH_UNITED_STATES,
+            actionName: {
+                [IETF_LOCALE.CHINESE_SIMPLIFIED]: "获取基础路径",
+                [IETF_LOCALE.ENGLISH_UNITED_STATES]: "Get Base Path",
+            },
+            actionDescription: {
+                [IETF_LOCALE.CHINESE_SIMPLIFIED]: "获取基础路径",
+                [IETF_LOCALE.ENGLISH_UNITED_STATES]: "Get Base Path",
+            },
+            parameters: [],
+            appearance: IMBRICATE_ORIGIN_ACTION_APPEARANCE.WARNING,
+        },
+    ];
+};
